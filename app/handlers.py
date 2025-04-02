4bf14f0ef10b618b6a62e01a25900c4af97af5df
iShawyha <shawyhaf@gmail.com>
2025-04-02 05:33:22 +0300

    add buttons "about us" and "donate". I will make support and mailing

--- app/handlers.py
+++ app/handlers.py
@@ -1,17 +1,61 @@
+import asyncio
+
 from aiogram import F, Router
 from aiogram.filters import CommandStart, Command
 from aiogram.types import Message, CallbackQuery
 
-from aiogram.fsm.state import State, StatesGroup
-from aiogram.fsm.context import FSMContext
-
 import app.database.requests as rq
 from app.services.parser import parser
 from app.services.generate_link import generate_link
+import app.keyboards as kb
 
 router = Router()
 
 @router.message(CommandStart())
 async def start(message: Message):
-    await rq.set_user(message.from_user.id)
-    await message.answer("СТАРТОВЕ ПОВІДОМЛЕННЯ ДОПИСАТИ НЕ ЗАБУДЬ")
+    if message.from_user.id == 1433760480:
+        await message.answer("О, ку!\nНа менюшку, може вона тобі треба)", reply_markup=kb.admin_main) 
+    else:
+        await rq.set_user(message.from_user.id)
+        await message.answer("""Вітаю в боті для перевірки конкурекції! 👋
+                                 
+Тут ми реалізували фільтрацію конкурентів для абітурієнтів(тобто майбуніх студентів😋),
+щоб ви не витрачали свій дорогоцінний час на однотипну роботу, яка, як правило, добре автоматизується!
+Ця програма буде корисна для тих,
+хто тільки подає заявки до університетів!
+
+P.s. Та має не 200 з усіх предметів НМТ..
+Для вас взагалі конкуренції не існує🫣
+                                
+                                😉Успіхів!✊
+                                """)
+        await message.answer("Ви в головному меню.\nДля координації по боту скористайтеся кнопками нижче👇", reply_markup=kb.user_main)
+
+
+@router.message(F.text == '❌ До головного меню')
+async def return_back(message: Message):
+            await message.answer("Ви в головному меню.\nДля координації по боту скористайтеся кнопками нижче👇", reply_markup=kb.user_main)
+            
+
+@router.message(F.text == '💸Донат💸')
+async def donate(message: Message):
+    await message.answer("""Сюди ви можете задонатити мені на новий ноутбук для навчання та програмування, буду дуже вдячний 🥰
+
+🎯 Ціль: 70 000 ₴
+
+🔗Посилання на банку
+https://send.monobank.ua/jar/23E3WYNesG
+
+💳Номер картки банки
+5375 4112 0596 9640
+                        """, reply_markup=kb.return_back)
+    
+    
+@router.message(F.text == "📑Про нас📑")
+async def about_us(message: Message):
+    await message.answer("Хтось взагалі натискає на цю кнопку?...", reply_markup=kb.remove_keyboard)
+    await asyncio.sleep(2)
+    await message.answer('Ну раз натиснули, значить цікаво)))')
+    await asyncio.sleep(1)
+    await message.answer('Тут тіпа шось написав щось дуже важне прям огого тут я допишу коли небудь шось про себе і взагалі цю програму, бо щас лєнь придумувати.\
+\nВзагалі я бідний студент, так що давайте якось задонатьте, чи що ', reply_markup=kb.about_us)
--- app/keyboards.py
+++ app/keyboards.py
@@ -1,2 +1,28 @@
-from aiogram.types import (ReplyKeyboardMarkup, KeyboardButton,
+from aiogram.types import (ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove,
                            InlineKeyboardMarkup, InlineKeyboardButton)
+
+remove_keyboard = ReplyKeyboardRemove()
+
+admin_main = ReplyKeyboardMarkup(keyboard=[
+    [KeyboardButton(text="📝Почати відсіювання!📝")],
+    [KeyboardButton(text='💸Донат💸'), KeyboardButton(text='📑Про нас📑')],
+    [KeyboardButton(text="📣Розсилка!")],
+], resize_keyboard=True)
+
+user_main = ReplyKeyboardMarkup(keyboard=[
+    [KeyboardButton(text="📝Почати відсіювання!📝")],
+    [KeyboardButton(text='💸Донат💸'), KeyboardButton(text='📑Про нас📑')],
+    [KeyboardButton(text="👤Зв'язок з адміністрацією👤")],
+], resize_keyboard=True)
+
+support = ReplyKeyboardMarkup(keyboard=[
+        [KeyboardButton(text="❌ До головного меню"), KeyboardButton(text="📤 Відправити")]
+], resize_keyboard=True)
+
+return_back = ReplyKeyboardMarkup(keyboard=[
+    [KeyboardButton(text='❌ До головного меню')]
+], resize_keyboard=True)
+
+about_us = ReplyKeyboardMarkup(keyboard=[
+    [KeyboardButton(text='💸Донат💸'), KeyboardButton(text='❌ До головного меню')]
+], resize_keyboard=True)
