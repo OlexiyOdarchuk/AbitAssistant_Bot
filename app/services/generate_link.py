47ccf1054426d38799949b0735910b25d302df30
iShawyha <shawyhaf@gmail.com>
2025-04-18 14:16:57 +0300

    Create LICENSE

--- app/services/generate_link.py
+++ app/services/generate_link.py
@@ -1,3 +1,18 @@
+# Copyright (c) 2025 iShawyha. All rights reserved.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import asyncio
 
 async def generate_link(name: str) -> str:
@@ -10,15 +25,15 @@ async def generate_link(name: str) -> str:
         str: Посилання на сайт abit-poisk.org
     """
     await asyncio.sleep(0)
-    parts = name.split() 
-    if len(parts) < 2: 
+    parts = name.split()
+    if len(parts) < 2:
         return "Помилка: Неправильно введене ім'я!!!"
-    surname = parts[0] 
-    first_initial = parts[1][0] 
-    if len(parts) > 2:  
-        middle_initial = parts[2][0] 
-        return f"https://abit-poisk.org.ua/#search-{surname}+{first_initial}+{middle_initial}" 
+    surname = parts[0]
+    first_initial = parts[1][0]
+    if len(parts) > 2:
+        middle_initial = parts[2][0]
+        return f"https://abit-poisk.org.ua/#search-{surname}+{first_initial}+{middle_initial}"
     elif len(parts) == 2:
-        return f"https://abit-poisk.org.ua/#search-{surname}+{first_initial}" 
+        return f"https://abit-poisk.org.ua/#search-{surname}+{first_initial}"
     else:
         return ValueError
--- app/services/parser.py
+++ app/services/parser.py
@@ -1,3 +1,18 @@
+# Copyright (c) 2025 iShawyha. All rights reserved.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import asyncio
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -23,7 +38,7 @@ async def save_to_file(driver, file_name: str) -> str:
     file_path = f'app/files/{file_name}.txt'
     with open(file_path, 'w') as write_file:
         write_file.write(driver.find_element(By.CLASS_NAME, "rwd-table").text)
-    
+
     return file_path
 
 async def parser(url: str, file_name: str) -> str:
